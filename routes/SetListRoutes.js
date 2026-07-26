b0faad8a5e6a74efcace757e33c6cf2e51a935b0
BoyAlone99935 <kingsleychijioke266@gmail.com>
2026-07-26 05:07:43 -0700

    adding performance videos

--- routes/SetListRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { getSetlistsByCelebrity } = require('../controllers/setListController.js')
-
-router.get('/celebrity/:id', getSetlistsByCelebrity)
-
-module.exports = router
