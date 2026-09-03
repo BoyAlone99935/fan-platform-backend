3e3fc8abec9d8f3524bf01dd99d928f606b75073
BoyAlone99935 <kingsleychijioke266@gmail.com>
2026-09-03 16:47:48 -0700

    add get ticket by purchase id route

--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const Auth = require('../middleware/Authenthication.js')
-const {createTicket , getAllPurchases , confirmPayment , getUserPurchases , getUnconfirmedPurchases } = require('../controllers/ticketController.js')
+const {createTicket , getAllPurchases , confirmPayment , getUserPurchases , getUnconfirmedPurchases , getTicketByPurchaseId } = require('../controllers/ticketController.js')
 
 router.post('/create-ticket' , Auth , createTicket)
 router.get("/all", Auth , getAllPurchases);
 router.get('/unconfirmed' , Auth ,  getUnconfirmedPurchases)
 router.get("/user", Auth ,  getUserPurchases);
 router.patch('/confirm' , Auth ,  confirmPayment)
-
+router.get("/ticket/:purchaseId", Auth, getTicketByPurchaseId);
 
 
 module.exports = router
