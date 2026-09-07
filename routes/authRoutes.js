d901ffd5f7c1035cda5f0a2520c7278496072c5d
BoyAlone99935 <kingsleychijioke266@gmail.com>
2026-09-07 11:46:04 -0700

    Initial commit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -21,13 +21,22 @@ router.post("/login", login);
 
 router.post("/logout", logout);
 
-router.get('/google' , 
+/*router.get('/google' , 
     passport.authenticate("google",{
       scope : ["profile" , "email"]
     })
-)
+)*/
 
-router.get('/google/callback' , 
+router.get('/google', (req, res, next) => {
+    const redirect = req.query.redirect || "/";
+
+    passport.authenticate("google", {
+        scope: ["profile", "email"],
+        state: redirect
+    })(req, res, next);
+});
+
+/*router.get('/google/callback' , 
     passport.authenticate("google" , {
         session: false,
         failureRedirect : "/login"
@@ -55,7 +64,38 @@ router.get('/google/callback' ,
     }
 
    
-)
+)*/
+
+
+
+router.get('/google/callback',
+    passport.authenticate("google", {
+        session: false,
+        failureRedirect: "/login"
+    }),
+
+    async (req, res) => {
+
+        const token = jwt.sign(
+            { userId: req.user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: "7d" }
+        );
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "lax",
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
+
+        const redirect = req.query.state || "/";
+
+        res.redirect(
+            `https://boyalone99935.github.io/user-frontend${redirect}`
+        );
+    }
+);
 
 
 
