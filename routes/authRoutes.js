8cd535cec354ebf396db345d8fbd2472ff54e55f
BoyAlone99935 <kingsleychijioke266@gmail.com>
2026-09-10 15:49:32 -0700

    Initial commit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -85,7 +85,7 @@ router.get('/google/callback',
         res.cookie("token", token, {
             httpOnly: true,
             secure: true,
-            sameSite: "lax",
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
