da99aabafe7cb3d3aa4627d5fbf17119cd6d3bd5
BoyAlone99935 <kingsleychijioke266@gmail.com>
2026-09-10 23:56:43 -0700

    Initial commit

--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -3,12 +3,12 @@ const router = express.Router()
 const Auth = require('../middleware/Authenthication.js')
 const {createTicket , getAllPurchases , confirmPayment , getUserPurchases , getUnconfirmedPurchases , getTicketByPurchaseId } = require('../controllers/ticketController.js')
 
-router.post('/create-ticket' , createTicket)
+router.post('/create-ticket' , Auth ,  createTicket)
 router.get("/all", Auth , getAllPurchases);
 router.get('/unconfirmed' , Auth ,  getUnconfirmedPurchases)
-router.get("/user", getUserPurchases);
+router.get("/user", Auth, getUserPurchases);
 router.patch('/confirm' , Auth ,  confirmPayment)
-router.get("/ticket/:purchaseId", getTicketByPurchaseId);
+router.get("/ticket/:purchaseId", Auth, getTicketByPurchaseId);
 
 
 module.exports = router
