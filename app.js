c9f75f89b5000d0749f351996b8d4ef32aa4b8a7
BoyAlone99935 <kingsleychijioke266@gmail.com>
2026-09-18 18:10:41 -0700

    Initial commit

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(cors({
   origin: [
     "http://localhost:5174",
     "http://localhost:5173",
-    "http://localhost:5175",
+    "https://headliner.cloud",
     "https://boyalone99935.github.io"
   ],
   credentials: true
